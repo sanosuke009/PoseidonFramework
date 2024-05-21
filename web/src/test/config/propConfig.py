5102aa71fc8ce5d9c2a2ff5aa4f4a0225e28c39d
Sudip <sanosuke009@gmail.com>
2024-05-21 21:11:16 +0530

    Trying to fix actions failure: Changed browser channel to webkit.

--- web/src/test/config/propConfig.py
+++ web/src/test/config/propConfig.py
@@ -2,8 +2,8 @@ localExecution = True # To check between local and vm/docker execution
 reportPath = 'allure-results/'
 implicitwait = 5000
 explicitwait = 5000
-browsername = 'chrome'
-headless = True
+browsername = 'webkit'
+headless = False
 browserargs = ["--start-maximized"]
 
 user_data_dir = "./web/user_data_dir"
